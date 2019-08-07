5a56df64fb8911e94730d4083765adc57d45408e
Rose Brennan <rose@grubhub.com>
2019-08-06 21:27:51 -0600

    setup moods container

---
+++ src/containers/MoodsContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MoodsContainer extends React.Component {
+
+  render() {
+    return(
+      <div>
+        MoodsContainer
+      </div>
+    )
+  }
+}
+
+export default MoodsContainer
