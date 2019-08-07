0fc686d8f09c164779f01f2734c048561e811169
Rose Brennan <rose@grubhub.com>
2019-08-06 21:33:07 -0600

    create component basics

---
+++ src/components/MoodInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MoodInput extends React.Component {
+
+  render() {
+    return (
+      <div>
+        MoodInput
+      </div>
+    )
+  }
+}
+
+export default MoodInput;
