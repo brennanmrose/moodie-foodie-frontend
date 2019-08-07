155ede474f054d5f95663de801deb2e7df3c9cca
Rose Brennan <rose@grubhub.com>
2019-08-07 12:12:51 -0600

    build out fetch request

--- src/actions/addMood.js
+++ src/actions/addMood.js
@@ -1,7 +1,14 @@
 export const addMood = (data) => {
 
   return (dispatch) => {
-
+    fetch('http://localhost:3000/api/v1/moods', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
   }
 
 }
