510b4abf250edaac0d430c59c46ac1859adee419
Rose Brennan <rose@grubhub.com>
2019-08-07 11:47:26 -0600

    add handleOnChange

--- src/components/MoodInput.js
+++ src/components/MoodInput.js
@@ -4,12 +4,19 @@ class MoodInput extends React.Component {
 
   state = { name: '' }
 
+  handleOnChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
   render() {
     return (
       <div>
         <form>
           <label>Create a New Mood: </label>
-          <input type='text'placeholder='Name' value={this.state.name}/>
+          <input type='text'placeholder='Name' value={this.state.name} name="name" onChange={this.handleOnChange}/><br></br>
+          <input type="submit" />
         </form>
       </div>
     )
