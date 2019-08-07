05f246d9cf2d4eaf55ec64b6ffe630112989e417
Rose Brennan <rose@grubhub.com>
2019-08-06 20:49:36 -0600

    setup mood reducer

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import moodReducer from './reducers/moodReducer'
 
 import App from './App';
 
-const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(moodReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/reducers/moodReducer.js
@@ -0,0 +1,5 @@
+export default function moodReducer(state = { moods: [] }, action) {
+
+  return state
+
+}
