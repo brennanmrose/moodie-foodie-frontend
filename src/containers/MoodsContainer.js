230ddb4ac017f7c9cd265e946ab6cd5a2c74084d
Rose Brennan <rose@grubhub.com>
2019-08-06 21:38:22 -0600

    handle moods and moodinput in app

--- src/containers/MoodsContainer.js
+++ src/containers/MoodsContainer.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Moods from '../components/Moods';
+import MoodInput from '../components/MoodInput';
+
 
 class MoodsContainer extends React.Component {
 
   render() {
     return(
       <div>
-        MoodsContainer
+        <MoodInput />
+        <Moods />
       </div>
     )
   }
