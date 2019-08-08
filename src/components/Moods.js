ce77acad1008a01981311e8d924a13c4f0f86be2
Rose Brennan <rose@grubhub.com>
2019-08-08 16:01:07 -0600

    pass props to mood

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Mood from './Mood';
 
 const Moods = (props) => {
 
   return (
     <div>
-      {props.moods.map(mood => <li key={mood.id}>{mood.name}</li>)}
+      {props.moods.map(mood => <li key={mood.id}><Mood mood={mood}/></li>)}
     </div>
   )
 
