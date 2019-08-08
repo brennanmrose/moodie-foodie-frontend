f27458487a5759dda7d622eafa6888180ccc45cd
Rose Brennan <rose@grubhub.com>
2019-08-08 16:41:10 -0600

    update show with dynamic url

--- src/components/Mood.js
+++ src/components/Mood.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Mood = (props) => {
 
+  let mood = props.moods[props.match.params.id - 1]
+
   return (
     <div>
-      {props.mood.name}
+      {mood ? mood.name : null}
     </div>
   )
 
