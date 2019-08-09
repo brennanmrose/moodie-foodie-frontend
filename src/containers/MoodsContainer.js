b08e96f36ae07d5e4f2be4721f28a02fb6ca87b5
Rose Brennan <rose@grubhub.com>
2019-08-09 15:49:48 -0600

    move all vituals out of MoodsContainer into VictualsContainer

--- src/containers/MoodsContainer.js
+++ src/containers/MoodsContainer.js
@@ -4,9 +4,6 @@ import { Route } from 'react-router-dom';
 
 import Mood from '../components/Mood';
 import Moods from '../components/Moods';
-import Foods from '../components/Foods';
-import Drinks from '../components/Drinks'
-import Desserts from '../components/Desserts'
 import MoodInput from '../components/MoodInput';
 import Home from '../components/Home'
 
@@ -19,9 +16,6 @@ class MoodsContainer extends React.Component {
         <Route exact path='/moods' render={ (routerProps) => <Moods {...routerProps} moods={this.props.moods} /> } />
         <Route path='/moods/new' component={MoodInput} />
         <Route exact path='/moods/:id' render={ (routerProps) => <Mood {...routerProps} moods={this.props.moods} /> } />
-        <Route path='/moods/:id/foods' render={ (routerProps) => <Foods {...routerProps} moods={this.props.moods} /> } />
-        <Route path='/moods/:id/drinks' render={ (routerProps) => <Drinks {...routerProps} moods={this.props.moods} /> } />
-        <Route path='/moods/:id/desserts' render={ (routerProps) => <Desserts {...routerProps} moods={this.props.moods} /> } />
       </div>
     )
   }
--- src/containers/VictualsContainer.js
+++ src/containers/VictualsContainer.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 
+import Foods from '../components/Foods';
+import Drinks from '../components/Drinks'
+import Desserts from '../components/Desserts'
+
 class VictualsContainer extends React.Component {
 
   render() {
     return (
-      Victuals
+      <div>
+        <Route path='/moods/:id/foods' render={ (routerProps) => <Foods {...routerProps} moods={this.props.moods} /> } />
+        <Route path='/moods/:id/drinks' render={ (routerProps) => <Drinks {...routerProps} moods={this.props.moods} /> } />
+        <Route path='/moods/:id/desserts' render={ (routerProps) => <Desserts {...routerProps} moods={this.props.moods} /> } />
+      </div>
     )
   }
 
