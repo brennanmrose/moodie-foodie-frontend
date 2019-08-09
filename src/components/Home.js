8f6bb33b73c642f13b128656369ead23d1ecd7f2
Rose Brennan <rose@grubhub.com>
2019-08-08 21:07:00 -0600

    initialize home component

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => (
+
+    <div>
+      <span>
+        Welcome to Moodie Foodie!
+      </span>
+    </div>
+
+);
+
+export default Home;
