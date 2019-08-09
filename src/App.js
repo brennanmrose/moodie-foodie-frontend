832ca29283d34f271f486815f53d490907a406b8
Rose Brennan <rose@grubhub.com>
2019-08-08 20:59:40 -0600

    add NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import MoodsContainer from './containers/MoodsContainer';
+import NavBar from './components/NavBar'
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
+        <NavBar />
         <MoodsContainer />
       </div>
     );
