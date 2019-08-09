0e96ec6874c9cf924dff52c79a7d491562e87076
Rose Brennan <rose@grubhub.com>
2019-08-08 21:36:41 -0600

    import foods

--- src/containers/MoodsContainer.js
+++ src/containers/MoodsContainer.js
@@ -5,6 +5,7 @@ import { Route } from 'react-router-dom';
 import { fetchMoods } from '../actions/fetchMoods';
 import Mood from '../components/Mood';
 import Moods from '../components/Moods';
+import Foods from '../components/Foods';
 import MoodInput from '../components/MoodInput';
 
 
@@ -19,7 +20,7 @@ class MoodsContainer extends React.Component {
       <div>
         <Route path='/moods/new' component={MoodInput} />
         <Route path='/moods/:id' render={ (routerProps) => <Mood {...routerProps} moods={this.props.moods} /> } />
-        <Route path='/moods/:id/foods' render={ (routerProps) => <Food {...routerProps} moods={this.props.moods} /> } />
+        <Route path='/moods/:id/foods' render={ (routerProps) => <Foods {...routerProps} moods={this.props.moods} /> } />
         <Route exact path='/moods' render={ (routerProps) => <Moods {...routerProps} moods={this.props.moods} /> } />
       </div>
     )
