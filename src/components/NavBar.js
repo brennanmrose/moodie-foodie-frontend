ab0c27573ec17cc5bb3b5b7b5225c80e6bfe2ff4
Rose Brennan <rose@grubhub.com>
2019-08-08 21:03:39 -0600

    update styling and add home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,8 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="NavBar">
-      <NavLink activeClassName="active" to='/moods' >Moods</NavLink>
+      <NavLink activeClassName="active" to='/home' >Home | </NavLink>
+      <NavLink activeClassName="active" to='/moods' >Moods | </NavLink>
       <NavLink activeClassName="active" to='/moods/new' >New Mood</NavLink>
     </div>
   )
