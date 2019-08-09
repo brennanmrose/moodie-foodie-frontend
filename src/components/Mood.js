cd276717993b4b5889b0289d5047280d928d6827
Rose Brennan <rose@grubhub.com>
2019-08-08 21:24:21 -0600

    display subcategories

--- src/components/Mood.js
+++ src/components/Mood.js
@@ -5,11 +5,17 @@ const Mood = (props) => {
   let mood = props.moods[props.match.params.id - 1]
 
   return (
+    mood ?
     <div>
-      {mood ? mood.name : null}
-    </div>
+      <h1>{mood.name}</h1>
+      <h3>Please select from the following:</h3>
+      <p>1. Foods</p>
+      <p>2. Drinks</p>
+      <p>3. Desserts</p>
+    </div> :
+    <p>There were no moods found with this id</p>
   )
 
 }
 
-export default Mood
+export default Mood;
