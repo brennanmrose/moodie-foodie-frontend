53e0b7897ad3711de14094f6c9978d3aaa975360
Rose Brennan <rose@grubhub.com>
2019-08-08 20:57:00 -0600

    initialize navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <div className="NavBar">
+      <NavLink activeClassName="active" to='/moods' >Moods</NavLink>
+      <NavLink activeClassName="active" to='/moods/new' >New Mood</NavLink>
+    </div>
+  )
+}
+
+export default NavBar;
