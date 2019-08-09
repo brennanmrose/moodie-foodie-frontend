9722d56e5252f2c644370929adf9aa55cedce1ed
Rose Brennan <rose@grubhub.com>
2019-08-08 21:48:28 -0600

    change name and build out display

---
+++ src/components/Foods.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Food = (props) => {
+
+  let mood = props.moods[props.match.params.id - 1]
+
+  return (
+    mood ?
+    <div>
+      <h1>Foods</h1>
+      <ul>
+        {mood.foods.map((food, index) => (
+          <li>
+            {food.name} - {food.description}
+          </li>
+        ))}
+      </ul>
+    </div>
+    : <p>There were no moods found with this id</p>
+  )
+
+}
+
+export default Food;
