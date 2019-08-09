0fcf74d158bce0325734fd21580a8e035694b14a
Rose Brennan <rose@grubhub.com>
2019-08-08 21:15:03 -0600

    add home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import MoodsContainer from './containers/MoodsContainer';
+import Home from './components/Home';
 import NavBar from './components/NavBar'
 
 class App extends React.Component {
@@ -9,6 +10,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <NavBar />
+        <Home />
         <MoodsContainer />
       </div>
     );
