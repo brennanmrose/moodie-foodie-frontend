e7a41a5ff3ace0b76bb3c7753cafc52489ac09e5
Rose Brennan <rose@grubhub.com>
2019-08-14 17:51:39 -0600

    add tabs variant and remove dropdown for now

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Nav, NavItem, Dropdown } from 'react-bootstrap';
+import { Nav, NavItem, Dropdown, NavDropdown } from 'react-bootstrap';
 
 const NavBar = () => {
   return (
     <div className="NavBar">
-      <Dropdown as={NavItem}>
-        <Dropdown.Toggle as={NavLink}>Menu</Dropdown.Toggle>
-        <Dropdown.Menu>
-          <Nav.Item>
-            <NavLink activeClassName="active" to='/home' >Home</NavLink>
-          </Nav.Item>
-          <Nav.Item>
-            <NavLink activeClassName="active" to='/moods' >Moods</NavLink>
-          </Nav.Item>
-          <Nav.Item>
-            <NavLink activeClassName="active" to='/moods/new' >New Mood</NavLink>
-          </Nav.Item>
-        </Dropdown.Menu>
-    </Dropdown>
+      <Nav variant="tabs" activeKey="1">
+      <Nav.Item>
+        <Nav.Link eventKey="1" href="#/home">
+          <NavLink activeClassName="active" to='/home' >Home</NavLink>
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="2" title="Item">
+          <NavLink activeClassName="active" to='/moods' >Moods</NavLink>
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="3" title="Item">
+          <NavLink activeClassName="active" to='/moods/new' >New Mood</NavLink>
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
     </div>
   )
 }
 
 export default NavBar;
+
+
+// <NavDropdown title="Dropdown" id="nav-dropdown">
+//   <NavDropdown.Item eventKey="4.1"><NavLink activeClassName="active" to='/home' >Home</NavLink></NavDropdown.Item>
+//   <NavDropdown.Item eventKey="4.2"><NavLink activeClassName="active" to='/moods' >Moods</NavLink></NavDropdown.Item>
+//   <NavDropdown.Item eventKey="4.3"><NavLink activeClassName="active" to='/moods/new' >New Mood</NavLink></NavDropdown.Item>
+//   <NavDropdown.Divider />
+// </NavDropdown>
