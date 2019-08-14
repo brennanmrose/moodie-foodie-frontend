597aeff0c0a7a6dab99832f5712c2d079b35cc02
Rose Brennan <rose@grubhub.com>
2019-08-14 17:41:43 -0600

    add bootstrap to navbar and create dropdown

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Nav, NavItem, Dropdown } from 'react-bootstrap';
 
 const NavBar = () => {
   return (
     <div className="NavBar">
-      <NavLink activeClassName="active" to='/home' >Home | </NavLink>
-      <NavLink activeClassName="active" to='/moods' >Moods | </NavLink>
-      <NavLink activeClassName="active" to='/moods/new' >New Mood</NavLink>
+      <Dropdown as={NavItem}>
+        <Dropdown.Toggle as={NavLink}>Menu</Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Nav.Item>
+            <NavLink activeClassName="active" to='/home' >Home</NavLink>
+          </Nav.Item>
+          <Nav.Item>
+            <NavLink activeClassName="active" to='/moods' >Moods</NavLink>
+          </Nav.Item>
+          <Nav.Item>
+            <NavLink activeClassName="active" to='/moods/new' >New Mood</NavLink>
+          </Nav.Item>
+        </Dropdown.Menu>
+    </Dropdown>
     </div>
   )
 }
