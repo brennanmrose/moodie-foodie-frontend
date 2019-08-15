fe60f36badc5dee549ae0158400efe92ed16577b
Rose Brennan <rose@grubhub.com>
2019-08-15 14:26:19 -0600

    update addFood action to be handled in mood reducer

--- src/reducers/moodReducer.js
+++ src/reducers/moodReducer.js
@@ -6,6 +6,13 @@ export default function moodReducer(state = { moods: [] }, action) {
     case 'ADD_MOOD':
       return {...state, moods: [...state.moods, action.payload]}
 
+    case 'ADD_FOOD':
+      let moodId = action.payload.id
+
+      return { ...state,
+        moods: state.moods.filter(mood => mood.id === moodId)
+      }
+
     default:
       return state
   }
