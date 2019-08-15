ee2ddd6a5ede0eae1aa7b5a99fa76f7a915e8cf7
Rose Brennan <rose@grubhub.com>
2019-08-15 16:15:34 -0600

    fix text colors

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => (
     <div>
       <span>
         <br></br>
-        <h1>Welcome to Moodie Foodie!</h1>
+        <h1 class="home">Welcome to Moodie Foodie!</h1>
         <p id="home">You can use this app to look up foods, drinks and desserts based on your current mood! You may also add new recipes to fit your unique preferences and build out a digital cookbook centered around your moods.</p>
       </span>
       <div class="image-holder">
