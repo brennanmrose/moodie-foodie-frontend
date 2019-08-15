8aef7461ce6203d1b2c5e8fc3aedfb6ed7712cc5
Rose Brennan <rose@grubhub.com>
2019-08-15 10:41:31 -0600

    add jumbotron

--- src/components/Mood.js
+++ src/components/Mood.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import '../Mood.css';
 import { Link } from 'react-router-dom';
+import { Jumbotron } from 'react-bootstrap';
 
 const Mood = (props) => {
 
  let mood = props.match ? props.moods[props.match.params.id - 1] : props.mood
+ let divStyle = {
+   color: 'blue',
+   backgroundImage: `${mood.image_url}`,
+ };
 
   return (
     mood ?
     <div>
-      <h1>{mood.name}</h1>
-      <h3>Please select from the following:</h3>
-      <Link to={`/moods/${mood.id}/foods`}>Foods </Link>
-      <br></br>
-      <br></br>
-      <Link to={`/moods/${mood.id}/drinks`}>Drinks </Link>
-      <br></br>
-      <br></br>
-      <Link to={`/moods/${mood.id}/desserts`}>Desserts </Link>
+      <Jumbotron fluid style={ divStyle }>
+        <h1>{mood.name}</h1>
+        <h3>Please select from the following:</h3>
+        <Link to={`/moods/${mood.id}/foods`}>Foods </Link>
+        <br></br>
+        <br></br>
+        <Link to={`/moods/${mood.id}/drinks`}>Drinks </Link>
+        <br></br>
+        <br></br>
+        <Link to={`/moods/${mood.id}/desserts`}>Desserts </Link>
+      </Jumbotron>
     </div> :
     null
   )
