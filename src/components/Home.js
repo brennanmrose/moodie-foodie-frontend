778d1c40286d7fc0d5a59758ff5390d003e789d5
Rose Brennan <rose@grubhub.com>
2019-08-15 13:21:18 -0600

    add text

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,9 @@ const Home = () => (
 
     <div>
       <span>
-        Welcome to Moodie Foodie!
+        <br></br>
+        <h1>Welcome to Moodie Foodie!</h1>
+        <p>You can use this app to look up foods, drinks and desserts based on your current mood! You may also add new recipes to fit your unique preferences and build out a digital cookbook centered around your moods.</p>
       </span>
     </div>
 
