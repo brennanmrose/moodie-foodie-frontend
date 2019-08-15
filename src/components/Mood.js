7124146021660ef48346f65baef189c8b2f2e6c5
Rose Brennan <rose@grubhub.com>
2019-08-15 11:45:13 -0600

    adjust jumbotron

--- src/components/Mood.js
+++ src/components/Mood.js
@@ -6,35 +6,34 @@ import { Jumbotron } from 'react-bootstrap';
 const Mood = (props) => {
 
  let mood = props.match ? props.moods[props.match.params.id - 1] : props.mood
- // let divStyle = {
- //   color: 'blue',
- //   backgroundImage: `${mood.image_url}`,
- // };
+
+ let divStyle = {
+   color: 'blue',
+   backgroundImage: `url(${mood && mood.image_url})`
+ };
 
   return (
     mood ?
-    <div>
-        <h1>{mood.name}</h1>
-        <h3>Please select from the following:</h3>
-        <Link to={`/moods/${mood.id}/foods`}>Foods </Link>
-        <br></br>
-        <br></br>
-        <Link to={`/moods/${mood.id}/drinks`}>Drinks </Link>
-        <br></br>
-        <br></br>
-        <Link to={`/moods/${mood.id}/desserts`}>Desserts </Link>
-    </div> :
-    null
+    <div class="container-full-bg">
+      <div class="jumbotron">
+        <div class="container">
+          <Jumbotron  fluid style={ divStyle }>
+            <h1>{mood.name}</h1>
+            <h3>Please select from the following:</h3>
+            <Link to={`/moods/${mood.id}/foods`}>Foods </Link>
+            <br></br>
+            <br></br>
+            <Link to={`/moods/${mood.id}/drinks`}>Drinks </Link>
+            <br></br>
+            <br></br>
+            <Link to={`/moods/${mood.id}/desserts`}>Desserts </Link>
+          </Jumbotron>
+        </div>
+      </div>
+    </div>
+     : null
   )
 
 }
 
 export default Mood;
-
-
-
-
-
-
-// <Jumbotron fluid style={ divStyle }>
-// </Jumbotron>
