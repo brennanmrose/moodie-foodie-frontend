4d5c222813f928f49febb8fa63112ee7eec1b2f6
Rose Brennan <rose@grubhub.com>
2019-08-15 14:30:19 -0600

    comment out food reducer

--- src/reducers/foodReducer.js
+++ src/reducers/foodReducer.js
@@ -1,10 +1,10 @@
-export default function foodReducer(state = { foods: [] }, action) {
-  switch (action.type) {
-    case 'ADD_FOOD':
-      return {...state, foods: [...state.foods, action.payload]}
-
-    default:
-      return state
-  }
-
-}
+// export default function foodReducer(state = { foods: [] }, action) {
+//   switch (action.type) {
+//     case 'ADD_FOOD':
+//       return {...state, foods: [...state.foods, action.payload]}
+//
+//     default:
+//       return state
+//   }
+//
+// }
