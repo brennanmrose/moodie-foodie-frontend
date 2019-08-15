078d7d0a4c1ee4a381304e3f2c62800aa553bf56
Rose Brennan <rose@grubhub.com>
2019-08-15 07:42:37 -0600

    comment out bootstrap

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import Mood from './Mood';
 import { Link } from 'react-router-dom';
+import { Dropdown, DropdownButton} from 'react-bootstrap'
 
 const Moods = (props) => {
 
   return (
     <div>
-      {props.moods.map(mood => <li key={mood.id}><Link to={`/moods/${mood.id}`}>{mood.name}</Link></li>)}
+    {props.moods.map(mood => <li key={mood.id}><Link to={`/moods/${mood.id}`}>{mood.name}</Link></li>)}
     </div>
   )
 
 }
 
 export default Moods;
+
+
+
+
+
+
+// <Dropdown>
+//   <Dropdown.Toggle variant="success" id="dropdown-basic">
+//     Dropdown Button
+//   </Dropdown.Toggle>
+//
+//   <Dropdown.Menu>
+//     {props.moods.map(mood => {
+//       <Dropdown.Item tag={ Link } tag={ Link } to={`/moods/${mood.id}`}>{mood.name}</Dropdown.Item>
+//     })}
+//   </Dropdown.Menu>
+// </Dropdown>
