93f03c18a615a8588ef766ebf08f446c723be75b
Rose Brennan <rose@grubhub.com>
2019-08-15 15:16:42 -0600

    add image to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,8 +10,15 @@ const Home = () => (
         <h1>Welcome to Moodie Foodie!</h1>
         <p id="home">You can use this app to look up foods, drinks and desserts based on your current mood! You may also add new recipes to fit your unique preferences and build out a digital cookbook centered around your moods.</p>
       </span>
+      <div class="image-holder">
+        <img src="https://d2ebzu6go672f3.cloudfront.net/media/content/images/p3_MedDiet_W1806_gi667751254.jpg" alt="Recipe" class="center" />
+      </div>
     </div>
 
 );
 
 export default Home;
+
+
+
+// <img src="https://d2ebzu6go672f3.cloudfront.net/media/content/images/p3_MedDiet_W1806_gi667751254.jpg" alt="Recipe" />
