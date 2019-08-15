46f0f8a3c1b1c374fc334f5cb101656bc2cd297c
Rose Brennan <rose@grubhub.com>
2019-08-15 14:35:04 -0600

    fix css to only apply to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => (
       <span>
         <br></br>
         <h1>Welcome to Moodie Foodie!</h1>
-        <p>You can use this app to look up foods, drinks and desserts based on your current mood! You may also add new recipes to fit your unique preferences and build out a digital cookbook centered around your moods.</p>
+        <p id="home">You can use this app to look up foods, drinks and desserts based on your current mood! You may also add new recipes to fit your unique preferences and build out a digital cookbook centered around your moods.</p>
       </span>
     </div>
 
