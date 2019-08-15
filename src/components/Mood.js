f6510d7514274809100e4c3189666f346c24e063
Rose Brennan <rose@grubhub.com>
2019-08-15 16:22:21 -0600

    update text color

--- src/components/Mood.js
+++ src/components/Mood.js
@@ -21,13 +21,13 @@ const Mood = (props) => {
             <h1 id="title"><span>{mood.name}</span></h1>
             <h3 id="text"><span>Please select from the following:</span></h3>
             <br></br>
-            <h4 id="text"><span>1. <Link to={`/moods/${mood.id}/foods`}>Foods </Link></span></h4>
+            <h4 id="text"><span>1. <Link to={`/moods/${mood.id}/foods`} class="victuals">Foods </Link></span></h4>
             <br></br>
             <br></br>
-            <h4 id="text"><span>2. <Link to={`/moods/${mood.id}/drinks`}>Drinks </Link></span></h4>
+            <h4 id="text"><span>2. <Link to={`/moods/${mood.id}/drinks`} class="victuals">Drinks </Link></span></h4>
             <br></br>
             <br></br>
-            <h4 id="text"><span>3. <Link to={`/moods/${mood.id}/desserts`}>Desserts </Link></span></h4>
+            <h4 id="text"><span>3. <Link to={`/moods/${mood.id}/desserts`} class="victuals">Desserts </Link></span></h4>
             <br></br>
             <br></br>
             <br></br>
