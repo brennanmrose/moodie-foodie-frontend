f92147eeb14e303c51cf96470fc127ef9386da82
Rose Brennan <rose@grubhub.com>
2019-08-18 14:12:40 -0600

    fix addFood reducer to work properly

--- src/reducers/moodReducer.js
+++ src/reducers/moodReducer.js
@@ -9,12 +9,28 @@ export default function moodReducer(state = { moods: [] }, action) {
     case 'ADD_FOOD':
       let moodId = action.payload.id
 
-      return { ...state,
-        moods: state.moods.filter(mood => mood.id === moodId)
-      }
+      let newMoods = state.moods.map(mood => {
+      	if (moodId === mood.id) {
+          return action.payload
+        } else {
+          return mood
+        }
+      });
+
+      return {...state, moods: newMoods}
 
     default:
       return state
   }
 
 }
+ // look for id from action creator and filter it out for delete
+
+// return { ...state,
+//   moods: state.moods.filter(mood => mood.id === moodId)
+// }
+
+// You will need to map through all of them and then have an if statement
+// to check the mood id and if they match, then you want to return the mood
+// that is coming in through the action but otherwise yuou want to return
+// the mood that is already in the array
