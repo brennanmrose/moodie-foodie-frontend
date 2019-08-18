21b85002a2c66d037cb534fa1113d12d242b79b0
Rose Brennan <rose@grubhub.com>
2019-08-18 15:29:21 -0600

    move drinkReducer into moodReducer

--- src/reducers/moodReducer.js
+++ src/reducers/moodReducer.js
@@ -21,16 +21,20 @@ export default function moodReducer(state = { moods: [] }, action) {
 
     default:
       return state
+
+    case 'ADD_DRINK':
+      let drinkMoodId = action.payload.id
+
+      let newDrinkMoods = state.moods.map(mood => {
+      	if (drinkMoodId === mood.id) {
+          return action.payload
+        } else {
+          return mood
+        }
+      });
+
+      return {...state, moods: newDrinkMoods}
   }
 
 }
  // look for id from action creator and filter it out for delete
-
-// return { ...state,
-//   moods: state.moods.filter(mood => mood.id === moodId)
-// }
-
-// You will need to map through all of them and then have an if statement
-// to check the mood id and if they match, then you want to return the mood
-// that is coming in through the action but otherwise yuou want to return
-// the mood that is already in the array
