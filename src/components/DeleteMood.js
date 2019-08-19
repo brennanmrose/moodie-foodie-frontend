c3c314a1548b1e398d35fb8a4f975a6d9b2c88b2
Rose Brennan <rose@grubhub.com>
2019-08-19 17:17:27 -0600

    build out delete mood component

---
+++ src/components/DeleteMood.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { deleteMood } from '../actions/deleteMood'
+
+class DeleteMood extends React.Component {
+
+  handleOnClick = () => {
+    this.props.deleteMood(this.props.mood)
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleOnClick}>Delete {this.props.mood.name}</button>
+      </div>
+    );
+  };
+
+};
+
+export default connect(null, { deleteMood })(DeleteMood);
