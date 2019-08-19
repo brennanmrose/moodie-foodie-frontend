cac5bec080b016750ac2296dd08b0995d0219a2b
Rose Brennan <rose@grubhub.com>
2019-08-19 16:51:53 -0600

    create deleteMood action

---
+++ src/actions/deleteMood.js
@@ -0,0 +1,13 @@
+export const deleteMood = (data) => {
+
+  return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/moods', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'DELETE',
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(mood => dispatch({ type: 'DELETE_MOOD', payload: mood, data }))
+  }
+
+}
